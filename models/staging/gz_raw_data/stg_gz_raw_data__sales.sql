9f36830d6fbdc90c7fc88bb21ca7facfeb9527f6
fereshteh.modaresi@gmail.com <fereshteh.modaresi@gmail.com>
2023-10-31 15:43:36 +0000

    test

--- models/staging/gz_raw_data/stg_gz_raw_data__sales.sql
+++ models/staging/gz_raw_data/stg_gz_raw_data__sales.sql
@@ -1,15 +1,22 @@
 with 
+
 source as (
+
     select * from {{ source('gz_raw_data', 'sales') }}
+
 ),
+
 renamed as (
+
     select
         date_date,
         orders_id,
-        pdt_id as products_id,  -- Corrected renaming syntax
-        CONCAT(orders_id, '_', pdt_id) AS unique_primary_key,  -- Use the original column name here
+        pdt_id, as products_id
         revenue,
         quantity
+
     from source
+
 )
+
 select * from renamed
