9f415083babf2c689606c5bde49f5abe16473bd1
fereshteh.modaresi@gmail.com <fereshteh.modaresi@gmail.com>
2023-10-31 12:40:37 +0000

    test

--- models/staging/gz_raw_data/stg_gz_raw_data__ship.sql
+++ models/staging/gz_raw_data/stg_gz_raw_data__ship.sql
@@ -1,26 +1,19 @@
 with 
 
 source as (
-
     select * from {{ source('gz_raw_data', 'ship') }}
-
 ),
 
 renamed as (
-
     select
         orders_id,
-        shipping_fee,
-        shipping_fee_1,
+        shipping_fee, -- keeping only this column
         logcost,
         ship_cost
-
     from source
+    where shipping_fee != shipping_fee_1  -- this WHERE clause will filter out rows where the two columns are different
+)
 
-
-
-where shipping_fee != shipping_fee_1)
- 
-select * from renamed
+select * from renamed;
 
  
