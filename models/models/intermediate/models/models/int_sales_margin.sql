c70a2924c7fd0314589da01bafae5f29272d854c
fereshteh.modaresi@gmail.com <fereshteh.modaresi@gmail.com>
2023-10-31 16:07:57 +0000

    test

---
+++ models/models/intermediate/models/models/int_sales_margin.sql
@@ -0,0 +1,27 @@
+WITH sales_data AS (
+    SELECT 
+        orders_id, 
+        products_id, 
+        date_date,
+        quantity, 
+        revenue 
+    FROM {{ source('gz_raw_data', 'sales') }}
+),
+
+product_data AS (
+    SELECT 
+        products_id, 
+        purchase_price
+    FROM {{ source('gz_raw_data', 'products') }}
+)
+
+SELECT 
+    s.orders_id, 
+    s.products_id,
+    s.date_date, 
+    s.quantity, 
+    s.revenue, 
+    p.purchase_price, 
+    s.quantity * p.purchase_price AS purchase_cost
+FROM sales_data s
+LEFT JOIN product_data p ON s.products_id = p.products_id
