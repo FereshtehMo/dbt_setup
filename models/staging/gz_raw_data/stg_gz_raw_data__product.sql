c89bd44d2d5ea2a5aead2282b99377f008244aab
fereshteh.modaresi@gmail.com <fereshteh.modaresi@gmail.com>
2023-10-31 15:50:29 +0000

    test

--- models/staging/gz_raw_data/stg_gz_raw_data__product.sql
+++ models/staging/gz_raw_data/stg_gz_raw_data__product.sql
@@ -1,19 +1,14 @@
-with 
-
-source as (
-
-    select * from {{ source('gz_raw_data', 'product') }}
-
+with source as (
+    select * from {{ source('gz_raw_data', 'sales') }}
 ),
-
 renamed as (
-
     select
-        products_id,
-        purchse_price as purchase_price
-
+        date_date,
+        orders_id,
+        pdt_id AS products_id,  -- Renaming syntax
+        CONCAT(orders_id, '_', pdt_id) AS unique_primary_key,  -- Concatenating orders_id and products_id
+        revenue,
+        quantity
     from source
-
 )
-
 select * from renamed
