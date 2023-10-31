f9a84c37d17dbda9c8a6328c1f2c406b8dae0f6a
fereshteh.modaresi@gmail.com <fereshteh.modaresi@gmail.com>
2023-10-31 12:02:51 +0000

    test

--- models/staging/gz_raw_data/stg_gz_raw_data__sales.sql
+++ models/staging/gz_raw_data/stg_gz_raw_data__sales.sql
@@ -1,25 +1,14 @@
-with        
-
+with 
 source as (
-    
     select * from {{ source('gz_raw_data', 'sales') }}
-
 ),
-
 renamed as (
-
     select
         date_date,
         orders_id,
-        pdt_id as products_id, -- renaming the column here
+        pdt_id as products_id,  -- Corrected renaming syntax
         revenue,
         quantity
-
     from source
-
 )
-
 select * from renamed
-
-
- 
