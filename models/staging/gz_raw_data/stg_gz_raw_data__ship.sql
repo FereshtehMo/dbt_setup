5dffacd7db01a43b77038099e5b81c32581b28cd
fereshteh.modaresi@gmail.com <fereshteh.modaresi@gmail.com>
2023-10-31 12:29:53 +0000

    test

--- models/staging/gz_raw_data/stg_gz_raw_data__ship.sql
+++ models/staging/gz_raw_data/stg_gz_raw_data__ship.sql
@@ -23,5 +23,15 @@ renamed as (
 
 select * from renamed
 
+diff_check as (
+    select
+        orders_id,
+        shipping_fee,
+        shipping_fee_1
+    from renamed
+    where shipping_fee <> shipping_fee_1
+    limit 10 -- limiting to preview first 10 rows if there are differences
+)
 
+select * from diff_check; -- this will show rows where shipping_fee and shipping_fee_1 are different
  
