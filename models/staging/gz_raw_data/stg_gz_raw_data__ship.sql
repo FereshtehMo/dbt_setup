d457b0c28c99d3b60d273868febbd679e643818c
fereshteh.modaresi@gmail.com <fereshteh.modaresi@gmail.com>
2023-10-31 12:28:25 +0000

    test

--- models/staging/gz_raw_data/stg_gz_raw_data__ship.sql
+++ models/staging/gz_raw_data/stg_gz_raw_data__ship.sql
@@ -24,11 +24,4 @@ renamed as (
 select * from renamed
 
 
-SELECT 
-    shipping_feea, 
-    shipping_fee_1 
-FROM 
-    {{ source('gz_raw_data', 'ship') }}
-WHERE 
-    shipping_feea <> shipping_fee_1 
-LIMIT 10; -- to preview only the first 10 different rows
+ 
