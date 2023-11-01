0c799acbf404c678cfc2fd6559008defa7de135e
fereshteh.modaresi@gmail.com <fereshteh.modaresi@gmail.com>
2023-11-01 12:16:58 +0000

    test

--- models/models/intermediate/models/models/int_sales_margin.sql
+++ models/models/intermediate/models/models/int_sales_margin.sql
@@ -1,27 +1,12 @@
-WITH sales_data AS (
-    SELECT 
-        orders_id, 
-        products_id, 
-        date_date,
-        quantity, 
-        revenue 
-    FROM {{ source('gz_raw_data', 'sales') }}  -- Corrected this line
-),
-
-product_data AS (
-    SELECT 
-        products_id, 
-        purchase_price
-    FROM {{ source('gz_raw_data', 'product') }}  -- Corrected this line
-)
-
 SELECT 
-    s.orders_id, 
-    s.products_id,
-    s.date_date, 
-    s.quantity, 
-    s.revenue, 
-    p.purchase_price, 
-    s.quantity * p.purchase_price AS purchase_cost
-FROM sales_data s
-LEFT JOIN product_data p ON s.products_id = p.products_id
+    products_id, 
+    date_date, 
+    orders_id,
+    revenue, 
+    quantity, 
+    purchase_price, 
+    ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
+    s.revenue - ROUND(s.quantity*p.purchase_price,2) AS margin
+FROM {{ref("stg_raw__sales")}} s
+LEFT JOIN {{ref("stg_raw__product")}} p 
+		USING (products_id)
