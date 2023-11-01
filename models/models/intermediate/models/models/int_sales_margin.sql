b1b0a95184be24bf145e3c52acbbf830c7a95c98
fereshteh.modaresi@gmail.com <fereshteh.modaresi@gmail.com>
2023-11-01 13:39:54 +0000

    test

--- models/models/intermediate/models/models/int_sales_margin.sql
+++ models/models/intermediate/models/models/int_sales_margin.sql
@@ -1,13 +1,13 @@
 SELECT 
 
-    s.products_id, 
-    s.date_date, 
-    s.orders_id,
-    s.revenue, 
-    s.quantity, 
-    p.purchase_price, 
-    ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
-    s.revenue - ROUND(s.quantity*p.purchase_price,2) AS margin
-FROM {{source('raw', 'sales')}} s
-LEFT JOIN {{source('raw', 'product')}} p 
-    ON s.pdt_id = p.products_id;
+    products_id, 
+    date_date, 
+    orders_id,
+    revenue, 
+    quantity, 
+    purchase_price, 
+    ROUND(CAST(s.quantity AS float64) *CAST( p.purchase_price AS float64) ,2) AS purchase_cost,
+    CAST (s.revenue AS float64) - ROUND (CAST(s.quantity AS float64) *CAST(p.purchase_price AS float64) ,2) AS margin
+FROM {{ref("stg_raw__sales")}} s
+LEFT JOIN {{ref ("stg_raw__product")}} p 
+    ON s.pdt_id = p.products_id
