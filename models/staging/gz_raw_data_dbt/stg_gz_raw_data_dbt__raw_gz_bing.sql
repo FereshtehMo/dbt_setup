114eb32d3ea51c40920abcd03537740f461862a4
fereshteh.modaresi@gmail.com <fereshteh.modaresi@gmail.com>
2023-11-01 14:26:40 +0000

    TEST

--- models/staging/gz_raw_data_dbt/stg_gz_raw_data_dbt__raw_gz_bing.sql
+++ models/staging/gz_raw_data_dbt/stg_gz_raw_data_dbt__raw_gz_bing.sql
@@ -13,7 +13,7 @@ renamed as (
         paid_source,
         campaign_key,
         campgn_name AS Campaign_name,
-        ads_cost,
+        CAST (ads_cost AS float64),
         impression,
         click
 
